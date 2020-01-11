f446fcc5d82626cc39e7d26942e86d5356f64517
poinwater <56426474+poinwater@users.noreply.github.com>
2020-01-10 16:45:27 -0800

    Module 1
    Module 1

---
+++ QueueClass/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueClass
+{
+   class Program
+   {
+      static void Show(Queue q)
+      { foreach (char c in q)
+         { 
+            Console.Write(c + " "); 
+         }
+         Console.WriteLine();
+      }
+
+      static void Main(string[] args)
+      {
+         Queue q = new Queue();
+
+         q.Enqueue('A');
+         q.Enqueue('M');
+         q.Enqueue('G');
+         q.Enqueue('W');
+
+         Console.WriteLine("Current queue: ");
+         Show(q);
+         Console.WriteLine();
+         q.Enqueue('V');
+         q.Enqueue('H');
+         Console.WriteLine("Current queue after adding V, H: ");
+         Show(q);
+
+
+         Console.WriteLine("Removing some values ");
+         char ch = (char)q.Dequeue();
+         Console.WriteLine("The removed value: {0}", ch);
+         ch = (char)q.Dequeue();
+         Console.WriteLine("The removed value: {0}", ch);
+         Console.WriteLine("Current queue: ");
+         Show(q);
+
+
+      }
+   }
+}
---
+++ StackClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace StackClass
+{
+   class Program
+   {
+
+      static void CurrentStack(Stack st)
+      {
+         Console.WriteLine("Current stack:");
+         foreach (char c in st)
+         {
+            Console.Write(c + " ");
+         }
+      }
+      static void Main(string[] args)
+      {
+         Stack st = new Stack();
+         st.Push('A');
+         st.Push('M');
+         st.Push('G');
+         st.Push('W');
+
+         CurrentStack(st);
+         Console.WriteLine();
+
+         st.Push('V');
+         st.Push('H');
+         Console.WriteLine("Push 'V' and 'H'");
+         Console.WriteLine("The next popable value in stack: {0}", st.Peek());
+ 
+
+  
+         CurrentStack(st);
+         Console.WriteLine();
+
+         Console.WriteLine("Remove 3 values: ");
+         st.Pop();
+         st.Pop();
+         st.Pop();
+         CurrentStack(st);
+
+
+      }
+   }
+}
