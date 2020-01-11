dff1afdb5de84b6be293be1af368c5ed719db3f0
poinwater <56426474+poinwater@users.noreply.github.com>
2020-01-10 16:43:07 -0800

    ArrayList
    Module1 ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListClass
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         ArrayList al = new ArrayList();
+         al.Add(45);
+         al.Add(78);
+         al.Add(33);
+         al.Add(56);
+         al.Add(12);
+         al.Add(23);
+         al.Add(9);
+
+         Console.WriteLine("Capacity:{0}",al.Capacity);
+         Console.WriteLine("Count:{0}", al.Count);
+
+         Console.WriteLine("Coutent: ");
+         foreach (int i in al)
+         {
+            Console.Write(i + " ");
+         }
+
+
+         Console.WriteLine();
+         Console.WriteLine("Sorted Coutent: ");
+         al.Sort();
+         foreach (int i in al)
+         {
+            Console.Write(i + " ");
+         }
+         Console.WriteLine();
+      }
+   }
+}
